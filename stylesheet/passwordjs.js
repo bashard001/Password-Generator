2db477be273447928e4f2848b9445db6f90b600e
bashard001 <bdaseh@gmail.com>
2019-10-06 22:11:36 -0700

    fixed the problem that the generater is not working properly the after using it once in the same session. also cleaned the code to use less ram

--- stylesheet/passwordjs.js
+++ stylesheet/passwordjs.js
@@ -12,17 +12,8 @@ var ucle = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 var lcle = "abcdefghijklmnopqrstuvwxyz";
 
-var hopefully = [];
-var newValues = "";
+
 var possiblePassword = '';
-
-var specialchar;
-var numberpass;
-var lowercase;
-var uppercase;
-
-
-
 
 
 
@@ -41,11 +32,12 @@ while ((passwordL < 8) ||
 
 
 }
+var hopefully = [];
 
-specialchar = confirm('do you want special characters?');
-numberpass = confirm('do you want numbers in the password?');
-lowercase = confirm('do you want lower case characters?');
-uppercase = confirm('do you want Uppercase characters?');
+var specialchar = confirm('do you want special characters?');
+var numberpass = confirm('do you want numbers in the password?');
+var lowercase = confirm('do you want lower case characters?');
+var uppercase = confirm('do you want Uppercase characters?');
 
     result.textContent = '';
     for (var i = 0; i < passwordL; i++) {
@@ -53,25 +45,23 @@ uppercase = confirm('do you want Uppercase characters?');
             var ucans = ucle.charAt(Math.floor(Math.random() * ucle.length));
             hopefully.push(ucans)
             console.log(ucans)
-        } else {ucans = ''; }
+        }
         if (lowercase === true) {
             var lcans = lcle.charAt(Math.floor(Math.random() * lcle.length));
             hopefully.push(lcans)
             console.log(lcans)
-        } else {lcans = '';}
+        } 
         if (specialchar === true) {
             var spans = letters.charAt(Math.floor(Math.random() * letters.length));
             hopefully.push(spans)
             console.log(spans)
-        } else {spans = '';}
+        } 
 
         if (numberpass === true) {
             var numans = nums.charAt(Math.floor(Math.random() * nums.length));
             hopefully.push(numans)
             console.log(numans)
-        } else {numans = '';}
-
-        //newValues = JSON.stringify(hopefully)
+        } 
 
         possiblePassword = hopefully[Math.floor(Math.random() * hopefully.length
         )]
